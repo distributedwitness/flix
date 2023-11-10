beb0a86b1efb25edfa44271fa70435190fb7865c
Distributed Witness <njgitdev+heroku@gmail.com>
2023-11-09 21:25:33 -0500

    Correct Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -178,40 +178,31 @@ Movie.create!([
 Genre.create!([
   {
     name: "Action"
-  }
-
+  },
   {
     name: "Comedy"
-  }
-
+  },
   {
     name: "Drama"
-  }
-
+  },
   {
     name: "Romance"
-  }
-
+  },
   {
     name: "Thriller"
-  }
-
+  },
   {
     name: "Fantasy"
-  }
-
+  },
   {
     name: "Documentary"
-  }
-
+  },
   {
     name: "Adventure"
-  }
-
+  },
   {
     name: "Animation"
-  }
-
+  },
   {
     name: "Sci-Fi"
   } 
